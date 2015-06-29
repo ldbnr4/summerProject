a6c8cf921dc90b82663233d52802cbfb915ce2e4
Lorenzo Boyice <lorenzo.boyice@garmin.com>
2015-06-29 07:30:30 -0500

    checking if bots work

--- ipBot.php
+++ ipBot.php
@@ -7,12 +7,29 @@ function getLocation( $ip ){
     @$dom->loadHTML( $html ); 
     $xpath = new DOMXPath($dom);
     $tableCol = $xpath->query('//td[@*]');
+    $ret = array();
     foreach ($tableCol as $col){
         if (strpos($col->nodeValue, 'postal_code')){
-            var_dump($col->nextSibling->nextSibling->nextSibling->nextSibling->nextSibling->nextSibling);
-            echo '<hr>';
+            $val = $col->nextSibling->nextSibling->nextSibling->nextSibling->nextSibling->nextSibling->textContent;
+            if(!in_array($val,$ret)){
+                array_push($ret,$val);
+            }
         }
-
+        else if (strpos($col->nodeValue, 'city')){
+            if($col->nextSibling->nextSibling->nextSibling->nextSibling){
+                $val = $col->nextSibling->nextSibling->nextSibling->nextSibling->nextSibling->nextSibling->textContent;
+                if(!in_array($val,$ret)){
+                    array_push($ret,$val);
+                }
+            }
+        }
+        if (strpos($col->nodeValue, 'region_name')){
+            $val = $col->nextSibling->nextSibling->nextSibling->nextSibling->nextSibling->nextSibling->textContent;
+            if(!in_array($val,$ret)){
+                array_push($ret,$val);
+            }
+        }
     }
+    return $ret;
 }
 ?>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -57,7 +57,29 @@
     include "../jamBaseBot.php";
     include "../ipBot.php";
     
-    getLocation('66.87.75.165');
+    function getRealIpAddr()
+                {
+                    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+                    {
+                      $ip=$_SERVER['HTTP_CLIENT_IP'];
+                    }
+                    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+                    {
+                      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+                    }
+                    else
+                    {
+                      $ip=$_SERVER['REMOTE_ADDR'];
+                    }
+                    return $ip;
+                }
+    
+    $location = getLocation(getRealIpAddr());
+    var_dump($location);
+    echo '<hr>';
+    $events =  getEvents( $location[1], $location[0], $location[2] );
+    var_dump($events);
+    
 ?>
     </div>
 	@yield('content')
