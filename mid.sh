c3245fc44b8ca0820288a99bfa1be3ad2c43ef72
Lorenzo Boyice <lorenzo.boyice@garmin.com>
2015-07-27 10:56:33 -0500

    trying another virtualenv

--- mid.sh
+++ mid.sh
@@ -2,7 +2,7 @@
 # use predefined variables to access passed arguments
 #echo arguments to the shell
 
-cd ENV/bin
+cd lilENV/bin
 source activate
 
 python -c "import pyJamBaseBot; pyJamBaseBot.getEvents(\"$1\"); "
