da165d86dbcac6cfa7700c2438a95a3c0523dab2
Lorenzo Boyice <lorenzo.boyice@garmin.com>
2015-07-27 11:04:09 -0500

    removing vritualenv

--- ENV/bin/pyDates.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from dateutil.relativedelta import relativedelta
-
-sDate = datetime.date.today().strftime("%m/%d/%Y")
-eDate =  datetime.date.today() + relativedelta(months=3)
-eDate = eDate.strftime("%m/%d/%Y")
-
-print sDate + ':' + eDate
