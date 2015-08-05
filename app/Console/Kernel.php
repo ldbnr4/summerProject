52449a3e59e026582e17ddfe1191baa3f6f7764d
Lorenzo Boyice <lorenzo.boyice@garmin.com>
2015-08-05 07:36:30 -0500

    cron job did not run over night changed to daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-        $schedule->command('Scripts:update')->dailyAt('23:00')->name('update_db')->withoutOverlapping();
+        $schedule->command('Scripts:update')->daily()->withoutOverlapping();
         
 	}
 
