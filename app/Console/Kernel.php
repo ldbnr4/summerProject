aae53d5db4f7a8dd67e8615b8a5c0d99e2cf8a37
Lorenzo Boyice <lorenzo.boyice@garmin.com>
2015-08-05 07:51:12 -0500

    making job run minutely

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-        $schedule->command('Scripts:update')->daily()->withoutOverlapping();
+        $schedule->command('Scripts:update')->cron('* * * * *')->withoutOverlapping();
         
 	}
 
